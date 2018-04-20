64fb759cc7e1fa7fd04ee3c3ac9db4ed7a159f22
Daniel <danielzs@kth.se>
2018-04-21 00:36:47 +0200

    Added NFA creation functions

--- Lab1/src/eNFAgraph/ENFA.java
+++ Lab1/src/eNFAgraph/ENFA.java
@@ -1,5 +1,8 @@
 package eNFAgraph;
 
+import java.util.*;
+import nfa.NFA;
+
 public class ENFA {
     public ENFAnode startNode;
     public ENFAnode acceptNode;
@@ -13,17 +16,70 @@ public class ENFA {
 
     public ENFAnode newNode(ENFAnode edge, String type) {
         if (type.length() == 1) {  // one-letter edge
-            return new ENFAnode(size++, edge, alphabet);
+            return new ENFAnode(getId(), edge, new Character[] {type.charAt(0)});
         } else if (type.equals("alphabet")) {
-            return new ENFAnode(size++, edge, alphabet);
+            return new ENFAnode(getId(), edge, alphabet);
         } else if (type.equals("epsilon")) {
-           return new ENFAnode(size++, edge);
+           return new ENFAnode(getId(), edge);
         } else throw new IllegalArgumentException("Allowed types: \"alphabet\", \"epsilon\" and any one-char string.");
     }
     public ENFAnode newNode() {
-        return new ENFAnode(size++);
+        return new ENFAnode(getId());
+    }
+    private int getId() { return size++; }
+
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        LinkedList<ENFAnode> queue = new LinkedList<>();
+        boolean[] alreadyQueuedNodes = new boolean[size];
+        queue.add(startNode);
+        alreadyQueuedNodes[startNode.id] = true;
+
+        while (!queue.isEmpty()) queue.remove().toStringBuilder(alreadyQueuedNodes, queue, str);
+
+        return str.toString();
     }
 
-    // Only get alphabet as a copy.
-//    public ArrayList<Character> alphabet() { return new ArrayList<>(alphabet); }
+    public NFA toNFA() {
+        Dictionary<Character, Set<Integer>>[] nfa = new Dictionary[size];
+        boolean[] acceptingStates = new boolean[size];
+
+        LinkedList<ENFAnode> queue = new LinkedList<>();
+        boolean[] alreadyQueuedNodes = new boolean[size];
+        queue.add(startNode);
+        alreadyQueuedNodes[startNode.id] = true;
+
+        while (!queue.isEmpty()) {
+            ENFAnode next = queue.remove();
+
+            // Fill edgeDict with edges reached from next.
+            Hashtable<Character, Set<ENFAnode>> edgeDict = new Hashtable<>(alphabet.length, 1);
+            for (Character c : alphabet) edgeDict.put(c, new LinkedHashSet<>());
+            boolean[] subAlreadyQueuedNodes = new boolean[size];  // Default values = false
+            subAlreadyQueuedNodes[next.id] = true;
+            next.getReachables(edgeDict, subAlreadyQueuedNodes, new LinkedList<>());
+
+            Dictionary<Character, Set<Integer>> nfaNode = new Hashtable<>(alphabet.length, 1);
+
+            // Fill queue with new reached edges, and put their ids into the nfa.NFA.
+            for (Character c : alphabet) {
+                Set<ENFAnode> reachedNodes = edgeDict.get(c);
+                Set<Integer> reachedNodeIds = new HashSet<>(reachedNodes.size());
+                for (ENFAnode reachedNode : reachedNodes) {
+                    if (!alreadyQueuedNodes[reachedNode.id]) {
+                        queue.add(reachedNode);
+                        alreadyQueuedNodes[reachedNode.id] = true;
+                    }
+                    reachedNodeIds.add(reachedNode.id);
+                }
+                nfaNode.put(c, reachedNodeIds);
+            }
+
+            nfa[next.id] = nfaNode;
+            if ( next.accepting ) acceptingStates[next.id] = true;
+        }
+
+        return new NFA(acceptingStates, startNode.id, nfa, alphabet);
+    }
+
 }
--- Lab1/src/eNFAgraph/ENFAnode.java
+++ Lab1/src/eNFAgraph/ENFAnode.java
@@ -1,8 +1,6 @@
 package eNFAgraph;
 
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.LinkedHashSet;
+import java.util.*;
 
 public class ENFAnode {
     public boolean accepting = false;
@@ -11,7 +9,7 @@ public class ENFAnode {
     public boolean hasLetterEdge;
     public ArrayList<ENFAnode> emptyEdges = new ArrayList<>();
 
-    // For NFA creation
+    // For nfa.NFA creation
     public int id;
 
     // Construct node without edges
@@ -35,12 +33,52 @@ public class ENFAnode {
         edgeLetters = null;
     }
 
+    protected void toStringBuilder(boolean[] alreadyQueuedNodes,
+                           LinkedList<ENFAnode> queue,
+                           StringBuilder str) {
+        str.append(String.format("%2d", id));
+        if (accepting) str.append("*||");
+        else str.append(" || ");
+        if ( !emptyEdges.isEmpty() ) str.append("ε");
+        for (ENFAnode epsEdge : emptyEdges) {
+            str.append("-");
+            str.append(epsEdge.id);
+            if (!alreadyQueuedNodes[epsEdge.id]) {
+                queue.add(epsEdge);
+                alreadyQueuedNodes[epsEdge.id] = true;
+            }
+        }
+        if ( hasLetterEdge ) {
+            if (edgeLetters.length == 1) str.append(edgeLetters[0]);
+            else str.append("all");
+//            str.append(edgeLetters[0]);
+            str.append("-");
+            str.append(edge.id);
+            if (!alreadyQueuedNodes[edge.id]) {
+                queue.add(edge);
+                alreadyQueuedNodes[edge.id] = true;
+            }
+        }
+        str.append("\n");
+    }
+
     public void addEmptyEdge(ENFAnode emptyEdge) {
         emptyEdges.add(emptyEdge);
     }
 
-    public void getReachables(Dictionary<Character, ArrayList<ENFAnode>> letteredEdgeDict) {
-
+    protected void getReachables(Dictionary<Character, Set<ENFAnode>> reachables,
+                                 boolean[] alreadyQueuedNodes,
+                                 LinkedList<ENFAnode> queue) {
+        if (hasLetterEdge) for (Character c : edgeLetters) reachables.get(c).add(edge);
+        for (ENFAnode epsEdge : emptyEdges)
+            if (!alreadyQueuedNodes[epsEdge.id]) {
+                queue.add(epsEdge);
+                alreadyQueuedNodes[epsEdge.id] = true;
+            }
+        if (!queue.isEmpty()) {
+            ENFAnode next = queue.remove();
+            next.getReachables(reachables, alreadyQueuedNodes, queue);
+        }
     }
 
 }
