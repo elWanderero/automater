b379ff2e6bcca02309fbf7e4b1b9149886535eaf
Daniel <danielzs@kth.se>
2018-05-01 21:53:11 +0200

    Working DFAgraph implementation.

--- Lab1/src/DFAgraph/DFA.java
+++ Lab1/src/DFAgraph/DFA.java
@@ -1,10 +1,53 @@
 package DFAgraph;
 
+import nfa.EquivalenceClass;
+
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Queue;
+
 public class DFA {
     public DFAnode start;
-    private DFAnode deathNode;
-//    private final Character[] alphabet;
+    public DFAnode deathNode;
+    private final Character[] alphabet;
+    private int size;
 
+    public DFA(Character[] alphabet) {
+        this.alphabet = alphabet;
+        size = 0;
+        deathNode = new DFAnode(alphabet, newId());
+    }
 
+    public DFAnode makeNode() {
+        return new DFAnode(alphabet, newId());
+    }
+
+    public void minimise() {
+        Map<DFAnode, Integer> classTable = new HashMap<>(size);
+    }
+
+    @Override
+    public String toString() {
+        if ( start == null ) return "[Empty DFA]";
+        StringBuilder str = new StringBuilder();
+        Queue<DFAnode> queue = new LinkedList<>();
+        boolean[] alreadyQueued = new boolean[size];
+        queue.add(start);
+        alreadyQueued[start.id] = true;
+        while ( !queue.isEmpty() ) {
+            DFAnode node = queue.remove();
+            node.addToStringBuilder(str);
+            for ( DFAnode edge : node.edges.values() ) {
+                if ( !alreadyQueued[edge.id] ) {
+                    queue.add(edge);
+                    alreadyQueued[edge.id] = true;
+                }
+            }
+        }
+        return str.toString();
+    }
+
+    private int newId() { return size++; }
 
 }
--- Lab1/src/DFAgraph/DFAnode.java
+++ Lab1/src/DFAgraph/DFAnode.java
@@ -1,8 +1,24 @@
 package DFAgraph;
 
-import java.util.Dictionary;
+import java.util.Map;
+import java.util.HashMap;
 
 public class DFAnode {
     public boolean accepting;
-    public Dictionary<Character, DFAnode> edges;
+    public Map<Character, DFAnode> edges;
+    public final int id;
+
+    public  DFAnode(Character[] alphabet, int id) {
+        this.edges = new HashMap<>(alphabet.length);
+        this.id = id;
+    }
+
+    void addToStringBuilder(StringBuilder str) {
+        str.append(String.format("%2d", id));
+        if (accepting) str.append("*||");
+        else str.append(" ||");
+        edges.forEach((c, node) -> str.append( node.id == 0 ? "" : String.format(" %c →%2d |", c, node.id)));
+        str.append(System.lineSeparator());
+    }
+
 }
