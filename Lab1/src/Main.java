0a60d144bcafb1d691f01c1fd4218f851667cee4
Daniel <danielzs@kth.se>
2018-05-01 21:49:06 +0200

    WIP

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -1,24 +1,30 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
+
+import DFAgraph.DFA;
 import eNFAgraph.ENFA;
+import nfa.NFA;
 import resyntax.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader input = new BufferedReader(new FileReader("./Lab1/tests/testcase2.txt"));
+        BufferedReader input = new BufferedReader(new FileReader("./Lab1/tests/testcase3.txt"));
 
         char[] tmp = input.readLine().toCharArray();
         Character[] alphabet = new Character[tmp.length];
         for (int i=0 ; i<tmp.length ;++i) alphabet[i] = tmp[i];
         RegExp regexRoot = REParser.parse(input.readLine());
         System.out.println(regexRoot.toString());
+
         ENFA eNFA = new ENFA(alphabet);
         regexRoot.toENFA(eNFA);
         eNFA.acceptNode.accepting = true;
-
         System.out.println(eNFA.toString());
-        eNFA.toNFA();
 
+        NFA nfa = eNFA.toNFA();
+        System.out.println(nfa.toString());
+        System.out.println();
+
+        DFA dfa = nfa.toDFA();
+        System.out.println(dfa.toString());
     }
 }
