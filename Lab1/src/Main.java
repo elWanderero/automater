871334da505ad63a6050102d09f7ec24439eddc8
Daniel <danielzs@kth.se>
2018-05-03 17:03:55 +0200

    WIP

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -7,24 +7,30 @@ import resyntax.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader input = new BufferedReader(new FileReader("./Lab1/tests/testcase3.txt"));
+        BufferedReader input = new BufferedReader(new FileReader("./Lab1/tests/testcase4.txt"));
 
         char[] tmp = input.readLine().toCharArray();
         Character[] alphabet = new Character[tmp.length];
         for (int i=0 ; i<tmp.length ;++i) alphabet[i] = tmp[i];
         RegExp regexRoot = REParser.parse(input.readLine());
-        System.out.println(regexRoot.toString());
+//        System.out.println(regexRoot.toString());
 
         ENFA eNFA = new ENFA(alphabet);
         regexRoot.toENFA(eNFA);
         eNFA.acceptNode.accepting = true;
-        System.out.println(eNFA.toString());
+//        System.out.println(eNFA.toString());
 
         NFA nfa = eNFA.toNFA();
-        System.out.println(nfa.toString());
-        System.out.println();
+//        System.out.println(nfa.toString());
+//        System.out.println();
 
         DFA dfa = nfa.toDFA();
-        System.out.println(dfa.toString());
+//        System.out.println(dfa.toString());
+
+        DFA minimalDFA = dfa.minimise();
+        System.out.println(minimalDFA.toString(false));
+
+        DFA minimalDFA2 = minimalDFA.minimise();
+        System.out.println(minimalDFA2.toString());
     }
 }
