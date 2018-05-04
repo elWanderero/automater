51b7d29a96364dea5cb6672a770d7db964464914
Daniel <danielzs@kth.se>
2018-05-04 12:25:00 +0200

    WIP

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -8,12 +8,12 @@ import resyntax.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader input = new BufferedReader(new FileReader("./Lab1/tests/testcase4.txt"));
-
         char[] tmp = input.readLine().toCharArray();
         Character[] alphabet = new Character[tmp.length];
         for (int i=0 ; i<tmp.length ;++i) alphabet[i] = tmp[i];
         RegExp regexRoot = REParser.parse(input.readLine());
-//        System.out.println(regexRoot.toString());
+        System.out.println(regexRoot.toString());
+
 
         ENFA eNFA = new ENFA(alphabet);
         regexRoot.toENFA(eNFA);
@@ -28,9 +28,16 @@ public class Main {
 //        System.out.println(dfa.toString());
 
         DFA minimalDFA = dfa.minimise();
-        System.out.println(minimalDFA.toString(false));
+//        System.out.println(minimalDFA.toString(false));
 
         DFA minimalDFA2 = minimalDFA.minimise();
         System.out.println(minimalDFA2.toString());
+
+        input.lines().forEachOrdered(
+                str -> {
+                    System.out.print(str);
+                    System.out.println(minimalDFA.eval(str) ? " YES!" : " nope");
+                }
+        );
     }
 }
