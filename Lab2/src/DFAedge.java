5491304748ed8bd8a031cae08d6f7b2543f4de19
Daniel <danielzs@kth.se>
2018-05-24 17:18:00 +0200

    Now implements Comparable

--- Lab2/src/DFAedge.java
+++ Lab2/src/DFAedge.java
@@ -1,4 +1,5 @@
-public class DFAedge {
+// Comparable so that we can sort collections of DFAedge, and search them faster.
+public class DFAedge implements Comparable<DFAedge> {
 
     public String q0;
     public String v;
@@ -8,5 +9,13 @@ public class DFAedge {
         this.q0 = q0;
         this.v = v;
         this.q1 = q1;
+    }
+
+    @Override
+    public int compareTo(DFAedge e2) {
+        if ( !q0.equals(e2.q0) ) return q0.compareTo(e2.q0);
+        if ( !v.equals(e2.v) ) return v.compareTo(e2.v);
+        if ( !q1.equals(e2.q1) ) return q1.compareTo(e2.q1);
+        return 0;
     }
 }
