dc734a3756ea44fe3f4bc3a3f4cfc5f4bd613f75
Daniel <danielzs@kth.se>
2018-06-05 13:39:58 +0200

    Added my own equals() and hashCode() so that it works as I intend with hashing structures.

--- Lab2/src/DFAedge.java
+++ Lab2/src/DFAedge.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 // Comparable so that we can sort collections of DFAedge, and search them faster.
 public class DFAedge implements Comparable<DFAedge> {
 
@@ -11,6 +13,7 @@ public class DFAedge implements Comparable<DFAedge> {
         this.q1 = q1;
     }
 
+    //  For efficient inclusion on structures. (Maybe not even needed if we use HashMaps!)
     @Override
     public int compareTo(DFAedge e2) {
         if ( !q0.equals(e2.q0) ) return q0.compareTo(e2.q0);
@@ -18,4 +21,15 @@ public class DFAedge implements Comparable<DFAedge> {
         if ( !q1.equals(e2.q1) ) return q1.compareTo(e2.q1);
         return 0;
     }
+
+    //  So that we can compare on content not object identity.
+    public boolean equals(DFAedge e2) { return q0.equals(e2.q0) && v.equals(e2.v) && q1.equals(e2.q1); }
+
+    //  So that DFAedges with equal contents get the same hash. This is necessary for
+    //  my usage of maps etc. to work as intended.
+    @Override
+    public int hashCode() {
+        return Objects.hash(q0, v, q1);
+    }
+
 }
